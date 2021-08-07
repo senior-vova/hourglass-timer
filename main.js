61f507d133c9cc2a026f2e1bce09dcc5c5733d13
senior-vova <vladimir.reg.acc@gmail.com>
2021-08-07 18:55:18 +0400

    Fix bug

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const timeInput = document.querySelector("#timeInput");
 const timeType = document.querySelector("#timeType");
-const timeOutput = document.querySelector(".time");
+const timeOutput = document.querySelector("#time");
 const startBtn = document.querySelector("#startBtn");
 const stopBtn = document.querySelector("#stopBtn");
 
@@ -11,41 +11,45 @@ const sandDown = document.querySelector(".bottom .sand-down");
 let timer = null;
 let startLeakSandTimeout = null;
 
+function generateAnimationValue(name, time, count) {
+    return `${name} ${time}s linear ${count} normal both`;
+}
+
 startBtn.addEventListener("click", () => {
     const timeTypeOption = timeType.value;
     let time = timeTypeOption == "minutes" ? timeInput.value * 60 : timeInput.value;
 
-    if(time && time > 0){
-        timeOutput.textContent = "00:00";
+    if(!isNaN(time) && time > 0){
         startBtn.disabled = true;
-        topSand.style.animation = `leak-sand ${time}s cubic-bezier(.5,.5,.6,.9) 1s infinite normal both`;
-        sandDown.style.animation = `sand-anim-1 1s cubic-bezier(.5,.5,.5,.5) 1s infinite normal both`;
+        stopBtn.disabled = false;
+        sandDown.style.animation = generateAnimationValue("jet-sand-anim", 1, 1);
+        topSand.style.animation = generateAnimationValue("leak-sand", time, 1);
         startLeakSandTimeout = setTimeout(()=>{
-            sandDown.style.animation = `sand-anim-2 ${time}s cubic-bezier(.5,.5,.6,.9) 2s infinite normal both`;
-            bottomSand.style.animation = `fill-sand ${time}s cubic-bezier(.5,.5,.5,.5) 1s infinite normal both`;
+            sandDown.style.animation = generateAnimationValue("jet-sand-second-anim", time, 1);
+            bottomSand.style.animation = generateAnimationValue("fill-sand", time, 1);
         }, 2000);
         timer = setInterval(()=>{
             timeOutput.textContent = formatTime(time);
             time--;
-            if(time <= 0){
-                clearData();
-            }
+            if(time < 0) clearData(true);
         }, 1000);
     }
 });
 
 stopBtn.addEventListener("click", () => {
-    clearData();
+    clearData(false);
 });
 
-const clearData = () => {
+const clearData = (showAlert = false) => {
     clearInterval(timer);
     clearTimeout(startLeakSandTimeout);
+    topSand.style.animation = ``;
+    bottomSand.style.animation = ``;
+    sandDown.style.animation = ``;
     timeOutput.textContent = "00:00";
-    topSand.style.animation = `none`;
-    bottomSand.style.animation = `none`;
-    sandDown.style.animation = `none`;
     startBtn.disabled = false;
+    stopBtn.disabled = true;
+    if (showAlert) alert("Time out"); 
 }
 
 const formatTime = (timeSeconds) => {
